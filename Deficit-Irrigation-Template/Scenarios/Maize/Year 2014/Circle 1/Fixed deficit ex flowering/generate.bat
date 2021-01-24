99abe60e3f540d4a6ce1bbe951560c003731143b
khanal18 <44209232+khanal18@users.noreply.github.com>
2021-01-23 17:39:56 -0700

    CropSyst sample data
    Sample data for CropSyst simulation (both corn and wheat for a year)

---
+++ Deficit-Irrigation-Template/Scenarios/Maize/Year 2014/Circle 1/Fixed deficit ex flowering/generate.bat
@@ -0,0 +1,12 @@
+..\..\..\..\..\adjirrig.exe Harvest.mgt 100 < optimal.dat
+..\..\..\..\..\adjirrig.exe Harvest.mgt 90  < optimal.dat
+..\..\..\..\..\adjirrig.exe Harvest.mgt 80  < optimal.dat
+..\..\..\..\..\adjirrig.exe Harvest.mgt 70  < optimal.dat
+..\..\..\..\..\adjirrig.exe Harvest.mgt 60  < optimal.dat
+..\..\..\..\..\adjirrig.exe Harvest.mgt 50  < optimal.dat
+..\..\..\..\..\adjirrig.exe Harvest.mgt 40  < optimal.dat
+..\..\..\..\..\adjirrig.exe Harvest.mgt 30  < optimal.dat
+..\..\..\..\..\adjirrig.exe Harvest.mgt 20  < optimal.dat
+..\..\..\..\..\adjirrig.exe Harvest.mgt 10  < optimal.dat
+..\..\..\..\..\adjirrig.exe Harvest.mgt 0  < optimal.dat
+
